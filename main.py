69b4af1096dcf2ca9577b6813e997e91346c9e3b
KingaPiasecka <piasecka.kingaa@gmail.com>
2018-06-19 19:47:10 +0200

    skeleton

--- main.py
+++ main.py
@@ -3,20 +3,30 @@ import cv2
 from segmentationAndScaling import segment_and_scale
 from imagePreprocessing import preprocessing
 from rotation import rotate
+from skeletonisation import skeletonize
+import numpy as np
 
 image = cv2.imread('ocr4.png')
 image = preprocessing(image)
 image = rotate(image)
 
 #show results
-cv2.imshow('rotated',image)
-cv2.waitKey()
+cv2.imshow('rotated', image)
 
-characters = segment_and_scale('training_image.png')
+
+##############################
+
+characters = segment_and_scale(image)
 img = characters[0]
-print len(characters)
-cv2.imshow('dst', img)
-cv2.moveWindow('dst', 40,30)
+cv2.imshow('img', img)
+cv2.moveWindow('img', 90, 90)
+
+skel = skeletonize(img)
+
+cv2.imshow('skel', skel)
+cv2.moveWindow('skel', 150, 90)
+cv2.imwrite("skel.png", skel)
+
+
 cv2.waitKey()
-
-
+cv2.destroyAllWindows()
---
+++ skeletonisation.py
@@ -0,0 +1,26 @@
+import numpy as np
+import scipy.ndimage.morphology as m
+
+def skeletonize(img):
+    h1 = np.array([[0, 0, 0], [0, 1, 0], [1, 1, 1]], dtype=np.uint8)
+    m1 = np.array([[1, 1, 1], [0, 0, 0], [0, 0, 0]], dtype=np.uint8)
+    h2 = np.array([[0, 0, 0], [1, 1, 0], [0, 1, 0]], dtype=np.uint8)
+    m2 = np.array([[0, 1, 1], [0, 0, 1], [0, 0, 0]], dtype=np.uint8)
+    hit_list = []
+    miss_list = []
+    for k in range(4):
+        hit_list.append(np.rot90(h1, k))
+        hit_list.append(np.rot90(h2, k))
+        miss_list.append(np.rot90(m1, k))
+        miss_list.append(np.rot90(m2, k))
+    img = img.copy()
+    while True:
+        last = img
+        for hit, miss in zip(hit_list, miss_list):
+            hm = m.binary_hit_or_miss(img, hit, miss)
+            img = np.logical_and(img, np.logical_not(hm))
+        if np.all(img == last):
+            break
+
+    img = img.astype(np.uint8) * 255
+    return img
